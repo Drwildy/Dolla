a86e39aec94017adf4096794854b45f4bcebf2da
Geordi Dosher <geordi.dosher@gmail.com>
2019-10-02 22:50:55 -0500

    EPIC-9: Fixing some hidden merge conflicts, downgrading to Angular-FontAwesome 0.2.0
    It appears each version of angular-fontawesome is different depending on your angular version.  0.2.0 is for Angular 6

--- DollaWeb/ClientApp/src/app/envelope.service.ts
+++ DollaWeb/ClientApp/src/app/envelope.service.ts
@@ -10,35 +10,37 @@ import { filter, map } from 'rxjs/operators';
 export class EnvelopeService {
 
   constructor(private http: HttpClient) { }
+
   addItemTest(envelope: Envelope) {
     console.log(envelope);
     console.log(this.http.post('/api/Envelopes', envelope)
       .subscribe());
     console.log("Added Envelope");
   }
+
   getEnvelopes(): Observable<Envelope[]> {
     let list: Array<Envelope> = [];
-    //list.push(<Envelope> {
-    //  id: 1,
-    //  name: "Fast Food",
-    //  createdDate: new Date(),
-    //  amount: 75.12,
-    //  setAmount: 100
-    //});
-    //list.push(<Envelope> {
-    //  id: 2,
-    //  name: "Groceries",
-    //  createdDate: new Date(),
-    //  amount: 15.59,
-    //  setAmount: 65
-    //});
-    //list.push(<Envelope> {
-    //  id: 3,
-    //  name: "Splurge",
-    //  createdDate: new Date(),
-    //  amount: 1234.48,
-    //  setAmount: 99
-    //});
+    list.push(<Envelope> {
+      id: 1,
+      name: "Fast Food",
+      createdDate: new Date(),
+      amount: 75.12,
+      setAmount: 100
+    });
+    list.push(<Envelope> {
+      id: 2,
+      name: "Groceries",
+      createdDate: new Date(),
+      amount: 15.59,
+      setAmount: 65
+    });
+    list.push(<Envelope> {
+      id: 3,
+      name: "Splurge",
+      createdDate: new Date(),
+      amount: 1234.48,
+      setAmount: 99
+    });
 
     return Observable.create(function (observer) {
       observer.next(list);
