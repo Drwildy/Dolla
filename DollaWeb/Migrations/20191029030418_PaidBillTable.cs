1d0372c529c999402f191614d1242f067cf5ac57
adrianp28 <tstewart11@uco.edu>
2019-10-28 22:07:57 -0500

    Added PaidBill Model, Ran Migration and added new Controller

---
+++ DollaWeb/Migrations/20191029030418_PaidBillTable.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DollaWeb.Migrations
+{
+    public partial class PaidBillTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaidBill",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BillId = table.Column<int>(nullable: true),
+                    TransactionId = table.Column<int>(nullable: true),
+                    Month = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaidBill", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PaidBill_MoneyBox_BillId",
+                        column: x => x.BillId,
+                        principalTable: "MoneyBox",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PaidBill_Transaction_TransactionId",
+                        column: x => x.TransactionId,
+                        principalTable: "Transaction",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaidBill_BillId",
+                table: "PaidBill",
+                column: "BillId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaidBill_TransactionId",
+                table: "PaidBill",
+                column: "TransactionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaidBill");
+        }
+    }
+}
---
+++ DollaWeb/Models/PaidBill.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DollaWeb.Models
+{
+    public class PaidBill
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Bill")]
+        public int? BillId { get; set; }
+        [ForeignKey("Transaction")]
+        public int? TransactionId { get; set; }
+        public string Month { get; set; }
+        public virtual Transaction Transaction { get; set; } //This is needed to recongize the ForeignKey in the migration
+        public virtual Bill Bill { get; set; } //This is needed to recongize the ForeignKey in the migration
+    }
+}
