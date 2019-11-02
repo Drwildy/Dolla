9f1e4ecdd46bc39d91d2d54791f1d8276a3d6760
trevorregier <35252050+trevorregier@users.noreply.github.com>
2019-11-02 04:46:20 -0500

    Initial push

--- DollaWeb/ClientApp/src/app/transactions/transactions.component.ts
+++ DollaWeb/ClientApp/src/app/transactions/transactions.component.ts
@@ -1,20 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { Transaction } from '../transaction';
 import { TransactionService } from '../transaction.service';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.css']
 })
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule
+  ]
+})
+
 export class TransactionsComponent implements OnInit {
 
-  transactions: Transaction[];
+  public transactions: Transaction[];
+  public mapping = new Map([
+    ["gas", "&#xf52f"],
+    ["fastfood", "&#xf805"],
+    ["school", "&#xf5d1"],
+    ["glasses", "&#xf530"],
+    ["books", "&#xf02d"]
+    // etc...
+  ]);
 
   constructor(private transactionService: TransactionService) {
-   this.transactions = [];
+    transactionService.getTransaction().forEach(t => {
+      this.transactions = t;
+    });
   }
 
+  getFromIcon(transaction: Transaction) {
+    /* 
+     * This function takes a transaction and
+     * returns the unicode for the corresponding
+     * Font Awesome icon for the sending account 
+     * to be used in an HTML template.
+     */
+
+    return this.mapping.get(this.transactions.find(t => {
+      t.id == transaction.transferToId
+    }).type);
+  }
+
+  getToIcon(transaction: Transaction) {
+    /* 
+     * This function takes a transaction and
+     * returns the unicode for the corresponding
+     * Font Awesome icon for the receiving account 
+     * to be used in an HTML template.
+     */
+
+    return this.mapping.get(this.transactions.find(t => {
+      t.id == transaction.transferToId
+    }).type);
+  }
 
   ngOnInit() {
     this.refresh();
