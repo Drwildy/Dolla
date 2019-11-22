715f4dde876571e38b7799dd2a1ebe946255816c
GeordiD <geordi.dosher@gmail.com>
2019-11-21 22:35:02 -0600

    Hiding overflow stuffs

--- DollaWeb/ClientApp/src/app/home-overview/home-overview.component.ts
+++ DollaWeb/ClientApp/src/app/home-overview/home-overview.component.ts
@@ -24,44 +24,44 @@ export class HomeOverviewComponent implements OnInit {
   ngOnInit() {
   }
 
-  overflowButton() {
-    console.log("it works");
+  //overflowButton() {
+  //  console.log("it works");
 
-    let envelopeName;
+  //  let envelopeName;
 
-    //gets envelopes
-    //get the amount of each envelope and add it
-    //curretly ahving issues getting the right amount just thought of an ingenious/spagetti way but no time lol
-    //this.envelopeService.getEnvelopeById(this.id)
-      //.subscribe((envelope: Envelope) => {
-        //this.envelopes = envelope;
+  //  //gets envelopes
+  //  //get the amount of each envelope and add it
+  //  //curretly ahving issues getting the right amount just thought of an ingenious/spagetti way but no time lol
+  //  //this.envelopeService.getEnvelopeById(this.id)
+  //    //.subscribe((envelope: Envelope) => {
+  //      //this.envelopes = envelope;
 
-        //console.log(this.myPiggyBank);
+  //      //console.log(this.myPiggyBank);
 
-      //});
+  //    //});
     
 
-    //adds a overflow bank
-    //currently just sets a hardcode amount due to get not working and month not implemented in this sprint
-    this.piggyName = "Overflow Month"
-    //lol random number generator
-    let randomNumber = Math.random() * 10;
-    let randomAmount = 10064.34 - randomNumber;
-    let myPiggyBank: Piggybank =
-    {
-      id: 0,
-      applicationUserId: "tstewart11",
-      name: this.piggyName,
-      createdDate: new Date(),
-      icon: this.selIconPiggy,
-      amount: randomAmount,
-      monthlyAllocation: 0
-    }
-    this.piggybankService.createPiggybank(myPiggyBank)
-      .subscribe(
-        result => {
-          this.piggybankService.dataChanged$.emit();
-        }
-      );
-  }
+  //  //adds a overflow bank
+  //  //currently just sets a hardcode amount due to get not working and month not implemented in this sprint
+  //  this.piggyName = "Overflow Month"
+  //  //lol random number generator
+  //  let randomNumber = Math.random() * 10;
+  //  let randomAmount = 10064.34 - randomNumber;
+  //  let myPiggyBank: Piggybank =
+  //  {
+  //    id: 0,
+  //    applicationUserId: "tstewart11",
+  //    name: this.piggyName,
+  //    createdDate: new Date(),
+  //    icon: this.selIconPiggy,
+  //    amount: randomAmount,
+  //    monthlyAllocation: 0
+  //  }
+  //  this.piggybankService.createPiggybank(myPiggyBank)
+  //    .subscribe(
+  //      result => {
+  //        this.piggybankService.dataChanged$.emit();
+  //      }
+  //    );
+  //}
 }
